b7111e9fe30e43fe1261252e0e9f787017b6ad6b
Ivan Kuzmichov <ivan.kuzmichov@gmail.com>
2020-04-16 20:18:25 +0300

    localization with parameters

--- src/js/components/localization.js
+++ src/js/components/localization.js
@@ -4,13 +4,24 @@ export default function() {
   Array.prototype.slice.call(els).forEach(item => {
     const msg = item.getAttribute('data-locale-message');
     if (!msg) return;
-    const translation = chrome.i18n.getMessage(msg);
-    if (!translation) return;
 
-    if (~msg.indexOf('placeholder')) {
-      item.placeholder = translation;
-      return;
+    // if exist params i18nString:param1,param2,param3
+    const params = msg.split(':');
+    if (params.length > 1) {
+      const arrString = params[1].split(',').map(str => {
+        return chrome.i18n.getMessage(str) || str;
+      });
+      item.textContent = chrome.i18n.getMessage(params[0], arrString);
+    } else {
+      // only string without params
+      const translation = chrome.i18n.getMessage(msg);
+      if (!translation) return;
+
+      if (~msg.indexOf('placeholder')) {
+        item.placeholder = translation;
+        return;
+      }
+      item.textContent = translation;
     }
-    item.textContent = translation;
   });
 }
